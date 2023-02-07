9fa6c950495ead7ce6ee0e195ed0e8415474c0ab
Yulia Shashkina <yula.v.s@yandex.ru>
2023-02-07 16:02:56 +0300

    feat/mobile version

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,59 @@
-//переменная кнопки эдит
+//Находим форму в DOM
+// Переменная кнопки
 let popupOpen = document.querySelector(".profile__edit-button-icon");
-// переменная окна
+// Окна
 let popup = document.querySelector(".popup");
-// переменная кнопки закрыть
+// Кнопки закрыть
 let popupClose = document.querySelector(".popup__close");
-// переменная формы
+// Формы
 let formElement = document.querySelector(".popup__form"); 
-// переменная  профиля имя b jgbcfybz
+// Профиля имя
 let profileName = document.querySelector(".profile__name");
-let profileDescription = document.querySelector(".profile__description");
+// Описания
+let profileJob = document.querySelector(".profile__job");
 
-//ввод имени и описания
+// Находим поля формы в DOM 
+//Ввод имени и описания
 let nameInput = document.querySelector(".popup__input_value_name");
-let descriptionInput = document.querySelector(".popup__input_value_description");
+let jobInput = document.querySelector(".popup__input_value_job");
 
-//функция открытия попап
+//f открытия попап
 function openPopup() {
   popup.classList.add(".popup_opened");
   nameInput.value = profileName.textContent;
-  descriptionInput.value = profileDescription.textContent;
+  jobInput.value = profileJob.textContent;
 }
-//функция закрытия попап
+//f закрытия попап
 function closePopup() {
   popup.classList.remove(".popup_opened");
   formElement.reset();
 }
-//функция редактирования формы
+//f редактирования формы
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
-  profileDescription.textContent = descriptionInput.value;
+  profileJob.textContent = jobInput.value;
   closePopup();
 }
 // слушатели событий 
 popupOpen.addEventListener('click', openPopup);
 popupClose.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
+
+
+// Находим форму в DOM
+let formElement = // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+let nameInput = // Воспользуйтесь инструментом .querySelector()
+let jobInput = // Воспользуйтесь инструментом .querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
